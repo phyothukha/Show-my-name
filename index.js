d205603a652e5e3de04608bd98f080f89fbe608a
phyothukha <phyothukha2193@gmail.com>
2022-11-04 20:54:25 +0630

    update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ export function showMyNameUi(){
     h1.style.position='fixed';
     h1.style.zIndex=1000;
     h1.style.fontSize=100+"px";
-    h1.style.color="aqua";
+    h1.style.color="white";
     h1.style.top=0;
     h1.style.right=0;
     h1.style.backgroundColor="black";
